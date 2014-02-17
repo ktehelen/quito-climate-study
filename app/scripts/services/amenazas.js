15126298a52450218c0236999d433a11939a0fd0
Alex Bedig <abedig@gmail.com>
2014-02-17 13:36:03 -0500

    initalized amenazas service => unclear from wireframe what content should accompany each of these, need clarification from SEI

--- app/scripts/services/amenazas.js
+++ app/scripts/services/amenazas.js
@@ -3,4 +3,17 @@
 angular.module('quitoClimateStudyApp')
   .service('Amenazas', function Amenazas() {
     //(threats)
+    //TODO: WHAT IS THE EXPECTED BEHAVIOR WHEN A USER CHOOSES ONE OF THE
+    //AMENAZAS FROM THE MENU ON THE LEFT?
+    var amenazas = [
+    	{
+    		name: 'Climáticas'
+    	},
+    	{
+    		name: 'Amenazas antropicas'
+    	},
+    	{
+    		name: 'Amenazas Morfoclimaticas'
+    	}
+    ];
   });

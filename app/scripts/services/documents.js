008e8f28023a4ae8f15d2a96887ec80d259500ca
Alex Bedig <abedig@gmail.com>
2014-02-17 13:27:35 -0500

    populated documents service with files specified on excel mockup

--- app/scripts/services/documents.js
+++ app/scripts/services/documents.js
@@ -3,4 +3,30 @@
 angular.module('quitoClimateStudyApp')
   .service('Documents', function Documents() {
     // (list of documents)
+    var documents = [
+    	{
+    		name: 'Analisis de Percepción',
+    		file: 'Filename of PDF to serve'
+    	},
+    	{
+    		name: 'Analisis integrado de amenazas cambio climático',
+    		file: 'Filename of PDF to serve'
+    	},
+    	{
+    		name: 'Estructura de metodologia',
+    		file: 'Filename of PDF to serve'
+    	},
+    	{
+    		name: 'Vulnerabilidad actural y futura e indices',
+    		file: 'Filename of PDF to serve'
+    	},
+    	{
+    		name: 'Lineas de adaptación',
+    		file: 'Filename of PDF to serve'
+    	},
+    	{
+    		name: 'Lecciones aprendidas',
+    		file: 'Filename of PDF to serve'
+    	},
+    ];
   });
